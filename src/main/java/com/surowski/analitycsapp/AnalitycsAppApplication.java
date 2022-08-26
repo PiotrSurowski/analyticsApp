94526bb6a4e2e1736f8626698b0a8c35412d02a9
Piotr Surowski <piotr.surowski248@gmail.com>
2022-08-26 20:25:01 +0200

    initial commit

---
+++ src/main/java/com/surowski/analitycsapp/AnalitycsAppApplication.java
@@ -0,0 +1,13 @@
+package com.surowski.analitycsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalitycsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnalitycsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/surowski/analitycsapp/AnalitycsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surowski.analitycsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalitycsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
